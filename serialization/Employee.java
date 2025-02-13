f54a0dd68fbde1ee10a4a14abc2fe0d5d743530b
Mishr1233 <shivshankarmishra84351935@gmail.com>
2025-02-13 14:59:56 +0530

    [shiv shankar] Add. a Java program that allows a user to store a list of employees in a file using Object Serialization and later retrieve the data from the file

---
+++ serialization/Employee.java
@@ -0,0 +1,41 @@
+package org.example.serialization;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    //Constructor
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    //Override toString() to display employee details
+    @Override
+    public String toString() {
+        return "Employee{id=" + id + ", name='" + name + "', department='" + department + "', salary=" + salary + "}";
+    }
+}
